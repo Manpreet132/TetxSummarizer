6214b09a2888d78787cb211b1d3b62b9206ee778
Manpreet Kullar <kullarmanpreetsingh@gmail.com>
2024-11-13 21:01:54 +0530

    Project completed

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -8,7 +8,7 @@ class ConfigurationManager:
                  config_path=CONFIG_FILE_PATH,
                  params_filepath=PARAMS_FILE_PATH):
         self.config=read_yaml(config_path)
-        #self.paramss=read_yaml(params_filepath)
+        self.params=read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
 
@@ -41,7 +41,7 @@ class ConfigurationManager:
     
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config=self.config.model_trainer
-        params=self.params.TrainingArguments
+        paramss=self.params.TrainingArguments
 
         create_directories([config.root_dir])
 
@@ -49,15 +49,15 @@ class ConfigurationManager:
             root_dir=config.root_dir,
             data_path=config.data_path,
             model_ckpt = config.model_ckpt,
-            num_train_epochs = params.num_train_epochs,
-            warmup_steps = params.warmup_steps,
-            per_device_train_batch_size = params.per_device_train_batch_size,
-            weight_decay = params.weight_decay,
-            logging_steps = params.logging_steps,
-            evaluation_strategy = params.evaluation_strategy,
-            eval_steps = params.evaluation_strategy,
-            save_steps = params.save_steps,
-            gradient_accumulation_steps = params.gradient_accumulation_steps
+            num_train_epochs = paramss.num_train_epochs,
+            warmup_steps = paramss.warmup_steps,
+            per_device_train_batch_size = paramss.per_device_train_batch_size,
+            weight_decay = paramss.weight_decay,
+            logging_steps = paramss.logging_steps,
+            evaluation_strategy = paramss.evaluation_strategy,
+            eval_steps = paramss.evaluation_strategy,
+            save_steps = paramss.save_steps,
+            gradient_accumulation_steps = paramss.gradient_accumulation_steps
         )
         return model_trainer_config
     
